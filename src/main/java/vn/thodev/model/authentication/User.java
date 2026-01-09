c1d9dc4405f40e08f1243c7cb7f6c7c7ab2e828a
thodev03 <thodev03@gmail.com>
2026-01-09 17:56:03 +0700

    chore(model): add relationship reward with user

--- src/main/java/vn/thodev/model/authentication/User.java
+++ src/main/java/vn/thodev/model/authentication/User.java
@@ -9,6 +9,7 @@ import vn.thodev.model.cart.Cart;
 import vn.thodev.model.customer.Customer;
 import vn.thodev.model.notification.Notification;
 import vn.thodev.model.review.Review;
+import vn.thodev.model.reward.RewardLog;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -69,5 +70,8 @@ public class User extends AbstractEntity<Long> {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Cart> carts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    List<RewardLog> rewardLogs = new ArrayList<>();
 
 }
