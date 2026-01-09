d84a0009a2ca169fa66b80f97a0c22241ac52502
thodev03 <thodev03@gmail.com>
2026-01-09 14:58:22 +0700

    chore(model): update guarantee field

--- src/main/java/vn/thodev/model/product/Guarantee.java
+++ src/main/java/vn/thodev/model/product/Guarantee.java
@@ -1,10 +1,15 @@
 package vn.thodev.model.product;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
 import lombok.experimental.Accessors;
 import vn.thodev.model.AbstractEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,9 +22,12 @@ public class Guarantee extends AbstractEntity<Long> {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "description", nullable = false)
+    @Column(name = "description")
     private String description;
 
     @Column(name = "status", nullable = false, columnDefinition = "SMALLINT")
     private Integer status;
+
+    @OneToMany(mappedBy = "guarantee",cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
 }
