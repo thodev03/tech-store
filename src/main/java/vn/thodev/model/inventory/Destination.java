1bcdffc5e3653c9b6f3f9b043b63b5ec8bcaa055
thodev03 <thodev03@gmail.com>
2026-01-09 19:50:14 +0700

    chore(model): add destination table

---
+++ src/main/java/vn/thodev/model/inventory/Destination.java
@@ -0,0 +1,38 @@
+package vn.thodev.model.inventory;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.Accessors;
+import vn.thodev.model.AbstractEntity;
+import vn.thodev.model.address.Address;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "destination")
+public class Destination extends AbstractEntity<Long> {
+    @Column(name = "contact_fullname")
+    private String contactFullname;
+
+    @Column(name = "contact_email")
+    private String contactEmail;
+
+    @Column(name = "contact_phone")
+    private String contactPhone;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id", referencedColumnName = "id", nullable = false, unique = true)
+    private Address address;
+
+    @Column(name = "status", nullable = false, columnDefinition = "TINYINT")
+    private Integer status;
+
+//    @OneToMany(mappedBy = "destination", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @JsonManagedReference
+//    private List<PurchaseOrder> purchaseOrders = new ArrayList<>();
+}
