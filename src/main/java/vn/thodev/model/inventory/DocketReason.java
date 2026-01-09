11eac1306f1f4406ba8fd72e4c32fb81d69d1c79
thodev03 <thodev03@gmail.com>
2026-01-09 19:49:25 +0700

    chore(model): add docket table

---
+++ src/main/java/vn/thodev/model/inventory/DocketReason.java
@@ -0,0 +1,17 @@
+package vn.thodev.model.inventory;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "docket_reason")
+public class DocketReason {
+}
---
+++ src/main/java/vn/thodev/model/inventory/DocketVariantKey.java
@@ -0,0 +1,14 @@
+package vn.thodev.model.inventory;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocketVariantKey {
+}
