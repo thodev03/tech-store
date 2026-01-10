423f546fa954818f8970e739bf666e6eb8bc4887
thodev03 <thodev03@gmail.com>
2026-01-10 15:06:40 +0700

    chore(model): add payment table

---
+++ src/main/java/vn/thodev/model/cashbook/PaymentMethod.java
@@ -0,0 +1,26 @@
+package vn.thodev.model.cashbook;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.Accessors;
+import vn.thodev.model.AbstractEntity;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment_method")
+public class PaymentMethod extends AbstractEntity<Long> {
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "code", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentMethodType code;
+
+    @Column(name = "status", nullable = false, columnDefinition = "TINYINT")
+    private Integer status;
+}
---
+++ src/main/java/vn/thodev/model/cashbook/PaymentMethodType.java
@@ -0,0 +1,6 @@
+package vn.thodev.model.cashbook;
+
+public enum PaymentMethodType {
+    CASH,
+    PAYPAL
+}
